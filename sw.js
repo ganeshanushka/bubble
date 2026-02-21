a53d6bc0c64bd2b5345b5b6d68727f7e254749e6
Anushka <anushkag@unc.edu>
2026-02-21 16:35:04 -0500

    Move close button further inside the circular bubble

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bubble-app-v10';
+const CACHE_NAME = 'bubble-app-v11';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
