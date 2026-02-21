be2c7406252caf83cb3122d0f63a8e995ae33593
Anushka <anushkag@unc.edu>
2026-02-21 16:55:06 -0500

    Simplify password requirements visual and logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bubble-app-v14';
+const CACHE_NAME = 'bubble-app-v15';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
